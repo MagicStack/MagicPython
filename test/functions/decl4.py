dd952d16465d15caf934b8fdd5dcfd29bddcc5a6
Victor Petrovykh <vpetrovykh@sprymix.com>
2015-10-09 22:27:31 -0400

    Add another test case for multiline annotation.

---
+++ test/functions/decl4.py
@@ -0,0 +1,55 @@
+# testing annotations split over multiple lines
+def some_func(a:
+                 lambda x=None:
+                    {key: val
+                        for key, val in
+                            (x if x is not None else [])
+                    }=42):
+
+
+
+
+#             : comment.line.number-sign.python, punctuation.definition.comment.python, source.python
+ testing annotations split over multiple lines : comment.line.number-sign.python, source.python
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+some_func     : entity.name.function.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+a             : meta.function.parameters.python, meta.function.python, source.python, variable.parameter.function.language.pyhton
+:             : meta.function.parameters.python, meta.function.python, punctuation.separator.annotation.python, source.python
+                  : meta.function.parameters.python, meta.function.python, source.python
+lambda        : meta.function.parameters.python, meta.function.python, meta.lambda-function.python, source.python, storage.type.function.lambda.python
+              : meta.function.lambda.parameters.python, meta.function.parameters.python, meta.function.python, meta.lambda-function.python, source.python
+x             : meta.function.lambda.parameters.python, meta.function.parameters.python, meta.function.python, meta.lambda-function.python, source.python, variable.parameter.function.language.pyhton
+=             : keyword.operator.python, meta.function.lambda.parameters.python, meta.function.parameters.python, meta.function.python, meta.lambda-function.python, source.python
+None          : constant.language.python, meta.function.lambda.parameters.python, meta.function.parameters.python, meta.function.python, meta.lambda-function.python, source.python
+:             : meta.function.parameters.python, meta.function.python, meta.lambda-function.python, punctuation.section.function.lambda.begin.python, source.python
+                     : meta.function.parameters.python, meta.function.python, source.python
+{             : meta.function.parameters.python, meta.function.python, source.python
+key: val      : meta.function.parameters.python, meta.function.python, source.python
+                         : meta.function.parameters.python, meta.function.python, source.python
+for           : keyword.operator.python, meta.function.parameters.python, meta.function.python, source.python
+ key, val     : meta.function.parameters.python, meta.function.python, source.python
+in            : keyword.operator.python, meta.function.parameters.python, meta.function.python, source.python
+                             : meta.function.parameters.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, source.python
+x             : meta.function.parameters.python, meta.function.python, source.python
+if            : keyword.operator.python, meta.function.parameters.python, meta.function.python, source.python
+ x            : meta.function.parameters.python, meta.function.python, source.python
+is            : keyword.operator.python, meta.function.parameters.python, meta.function.python, source.python
+              : meta.function.parameters.python, meta.function.python, source.python
+not           : keyword.operator.python, meta.function.parameters.python, meta.function.python, source.python
+              : meta.function.parameters.python, meta.function.python, source.python
+None          : constant.language.python, meta.function.parameters.python, meta.function.python, source.python
+              : meta.function.parameters.python, meta.function.python, source.python
+else          : keyword.operator.python, meta.function.parameters.python, meta.function.python, source.python
+              : meta.function.parameters.python, meta.function.python, source.python
+[             : meta.function.parameters.python, meta.function.python, source.python
+]             : meta.function.parameters.python, meta.function.python, source.python
+)             : meta.function.parameters.python, meta.function.python, source.python
+                     : meta.function.parameters.python, meta.function.python, source.python
+}             : meta.function.parameters.python, meta.function.python, source.python
+=             : keyword.operator.assignment.python, meta.function.parameters.python, meta.function.python, source.python
+42            : constant.numeric.dec.python, meta.function.parameters.python, meta.function.python, source.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
