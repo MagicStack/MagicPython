93c914a0537ee0665e5139e8a8a8bc9508a25dd7
Yury Selivanov <yselivanov@sprymix.com>
2015-10-13 00:04:32 -0400

    Add a test for .format() method

--- test/strings/format2.py
+++ test/strings/format2.py
@@ -1,4 +1,4 @@
-"normal {{ normal }} normal {fo.__add__!s}"
+"normal {{ normal }} normal {fo.__add__!s}".format(fo=1)
 
 
 "             : source.python, string.quoted.double.python
@@ -11,3 +11,10 @@ normal        : source.python, string.quoted.double.python
 !s            : constant.character.format.python, source.python, storage.type.format.python, string.quoted.double.python
 }             : constant.character.format.python, source.python, string.quoted.double.python
 "             : source.python, string.quoted.double.python
+.             : source.python
+format        : meta.function-call.python, source.python
+(             : meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+fo            : meta.function-call.arguments.python, meta.function-call.python, source.python, variable.parameter.function-call.pyhton
+=             : keyword.operator.assignment.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+1             : constant.numeric.dec.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+)             : meta.function-call.python, punctuation.definition.arguments.end.python, source.python
