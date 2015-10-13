59b5001591bf00b20c4589b40bad2ad4b298e36c
Yury Selivanov <yselivanov@sprymix.com>
2015-10-13 00:27:43 -0400

    Highlight illegal (True, False, None) class and function names

---
+++ test/classes/class8.py
@@ -0,0 +1,30 @@
+class None: pass
+class True(False): pass
+class False(object): pass
+
+
+
+class         : meta.class.python, source.python, storage.type.class.python
+              : meta.class.python, source.python
+None          : invalid.illegal.name.python, meta.class.python, source.python
+:             : meta.class.python, punctuation.section.class.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
+class         : meta.class.python, source.python, storage.type.class.python
+              : meta.class.python, source.python
+True          : invalid.illegal.name.python, meta.class.python, source.python
+(             : meta.class.inheritance.python, meta.class.python, punctuation.definition.inheritance.begin.python, source.python
+False         : constant.language.python, meta.class.inheritance.python, meta.class.python, source.python
+)             : meta.class.inheritance.python, meta.class.python, punctuation.definition.inheritance.end.python, source.python
+:             : meta.class.python, punctuation.section.class.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
+class         : meta.class.python, source.python, storage.type.class.python
+              : meta.class.python, source.python
+False         : invalid.illegal.name.python, meta.class.python, source.python
+(             : meta.class.inheritance.python, meta.class.python, punctuation.definition.inheritance.begin.python, source.python
+object        : meta.class.inheritance.python, meta.class.python, source.python, support.type.python
+)             : meta.class.inheritance.python, meta.class.python, punctuation.definition.inheritance.end.python, source.python
+:             : meta.class.python, punctuation.section.class.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
---
+++ test/functions/decl6.py
@@ -0,0 +1,30 @@
+def True(): pass
+def None(): pass
+def False(): pass
+
+
+
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+True          : invalid.illegal.name.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+None          : invalid.illegal.name.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+False         : invalid.illegal.name.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
