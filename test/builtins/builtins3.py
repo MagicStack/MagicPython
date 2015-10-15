9a4736a9552c576c0c3a79ee5495e2f7ee65dc22
Victor Petrovykh <vpetrovykh@sprymix.com>
2015-10-15 12:54:00 -0400

    Add test for builtin variables.

---
+++ test/builtins/builtins3.py
@@ -0,0 +1,87 @@
+__all__ = ['bar', 'baz']
+some.__bases__
+some.__class__
+assert __debug__
+some.__dict__
+some.__doc__
+some.__file__
+some.__members__
+some.__metaclass__
+some.__methods__
+some.__module__
+some.__mro__
+some.__name__
+some.__slots__
+some.__subclasses__
+some.__version__
+some.__weakref__
+some.__notspecial__
+
+
+
+
+__all__       : source.python, support.variable.magic.python
+              : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+[             : punctuation.definition.list.begin.python, source.python
+'             : punctuation.definition.string.begin.python, source.python, string.quoted.single.python
+bar           : source.python, string.quoted.single.python
+'             : punctuation.definition.string.end.python, source.python, string.quoted.single.python
+,             : source.python
+'             : punctuation.definition.string.begin.python, source.python, string.quoted.single.python
+baz           : source.python, string.quoted.single.python
+'             : punctuation.definition.string.end.python, source.python, string.quoted.single.python
+]             : punctuation.definition.list.end.python, source.python
+some          : source.python
+.             : source.python
+__bases__     : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__class__     : source.python, support.variable.magic.python
+assert        : keyword.control.flow.python, source.python
+              : source.python
+__debug__     : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__dict__      : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__doc__       : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__file__      : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__members__   : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__metaclass__ : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__methods__   : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__module__    : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__mro__       : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__name__      : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__slots__     : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__subclasses__ : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__version__   : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__weakref__   : source.python, support.variable.magic.python
+some          : source.python
+.             : source.python
+__notspecial__ : source.python
+              : source.python
