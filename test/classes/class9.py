c0daabfde500c1e449b1604818a9056efc8b9e5a
Yury Selivanov <yselivanov@sprymix.com>
2015-10-14 23:38:02 -0400

    Add tests for dunder names

---
+++ test/classes/class9.py
@@ -0,0 +1,16 @@
+class Foo:
+    __slots__ = ()
+
+
+
+class         : meta.class.python, source.python, storage.type.class.python
+              : meta.class.python, source.python
+Foo           : entity.name.type.class.python, meta.class.python, source.python
+:             : meta.class.python, punctuation.section.class.begin.python, source.python
+              : source.python
+__slots__     : source.python, support.variable.magic.python
+              : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+(             : punctuation.parenthesis.begin.python, source.python
+)             : punctuation.parenthesis.end.python, source.python
---
+++ test/expressions/special1.py
@@ -0,0 +1,11 @@
+__version__ __doc__ __file__
+__author__
+
+
+
+__version__   : source.python, support.variable.magic.python
+              : source.python
+__doc__       : source.python, support.variable.magic.python
+              : source.python
+__file__      : source.python, support.variable.magic.python
+__author__    : source.python
