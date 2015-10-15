594cbe4170b98394642a937a54ecc910962d6113
Yury Selivanov <yselivanov@sprymix.com>
2015-10-15 00:24:38 -0400

    Allow some whitespace in decorators

---
+++ test/functions/decorators2.py
@@ -0,0 +1,20 @@
+@ f . bar
+def foo(): pass
+
+
+
+@             : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+              : meta.function.decorator.python, source.python
+f             : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+              : meta.function.decorator.python, source.python
+.             : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+              : meta.function.decorator.python, source.python
+bar           : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+foo           : entity.name.function.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
---
+++ test/functions/decorators3.py
@@ -0,0 +1,28 @@
+@ f . bar (baz = 1)
+def foo(): pass
+
+
+
+@             : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+              : meta.function.decorator.python, source.python
+f             : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+              : meta.function.decorator.python, source.python
+.             : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+              : meta.function.decorator.python, source.python
+bar           : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+              : meta.function.decorator.python, source.python
+(             : meta.function-call.arguments.python, meta.function.decorator.python, punctuation.definition.arguments.begin.python, source.python
+baz           : meta.function-call.arguments.python, meta.function.decorator.python, source.python, variable.parameter.function-call.pyhton
+              : meta.function-call.arguments.python, meta.function.decorator.python, source.python
+=             : keyword.operator.assignment.python, meta.function-call.arguments.python, meta.function.decorator.python, source.python
+              : meta.function-call.arguments.python, meta.function.decorator.python, source.python
+1             : constant.numeric.dec.python, meta.function-call.arguments.python, meta.function.decorator.python, source.python
+)             : meta.function.decorator.python, punctuation.definition.arguments.end.python, source.python
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+foo           : entity.name.function.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
