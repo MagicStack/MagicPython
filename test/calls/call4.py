6327b745fe8fc514da3cc09c9f3be4dfc6477c80
Victor Petrovykh <vpetrovykh@sprymix.com>
2015-10-15 13:23:59 -0400

    Highlight chain of function calls.

---
+++ test/calls/call4.py
@@ -0,0 +1,31 @@
+foo.1
+foo(bar=baz)(ham=sam)
+foo.None and foo.None.baz
+
+
+
+foo           : source.python
+.             : source.python
+1             : source.python
+              : source.python
+foo           : meta.function-call.python, source.python
+(             : meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+bar           : meta.function-call.arguments.python, meta.function-call.python, source.python, variable.parameter.function-call.pyhton
+=             : keyword.operator.assignment.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+baz           : meta.function-call.arguments.python, meta.function-call.python, source.python
+)             : meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.arguments.end.python, source.python
+(             : meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+ham           : meta.function-call.arguments.python, meta.function-call.python, source.python, variable.parameter.function-call.pyhton
+=             : keyword.operator.assignment.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+sam           : meta.function-call.arguments.python, meta.function-call.python, source.python
+)             : meta.function-call.python, punctuation.definition.arguments.end.python, source.python
+foo           : source.python
+.             : source.python
+None          : keyword.invalid.illegal.name.python, source.python
+              : source.python
+and           : keyword.operator.python, source.python
+ foo.         : source.python
+None          : constant.language.python, source.python
+.             : source.python
+baz           : source.python
+              : source.python
---
+++ test/classes/class10.py
@@ -0,0 +1,30 @@
+class Foo(Bar(q=1) (w=2) (e=3)): pass
+
+
+
+class         : meta.class.python, source.python, storage.type.class.python
+              : meta.class.python, source.python
+Foo           : entity.name.type.class.python, meta.class.python, source.python
+(             : meta.class.inheritance.python, meta.class.python, punctuation.definition.inheritance.begin.python, source.python
+Bar           : entity.other.inherited-class.pyhton, meta.class.inheritance.python, meta.class.python, meta.function-call.python, source.python
+(             : meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+q             : meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, source.python, variable.parameter.function-call.pyhton
+=             : keyword.operator.assignment.python, meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+1             : constant.numeric.dec.python, meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+)             : meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.arguments.end.python, source.python
+              : meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+(             : meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+w             : meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, source.python, variable.parameter.function-call.pyhton
+=             : keyword.operator.assignment.python, meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+2             : constant.numeric.dec.python, meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+)             : meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.arguments.end.python, source.python
+              : meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+(             : meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+e             : meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, source.python, variable.parameter.function-call.pyhton
+=             : keyword.operator.assignment.python, meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+3             : constant.numeric.dec.python, meta.class.inheritance.python, meta.class.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+)             : meta.class.inheritance.python, meta.class.python, meta.function-call.python, punctuation.definition.arguments.end.python, source.python
+)             : meta.class.inheritance.python, meta.class.python, punctuation.definition.inheritance.end.python, source.python
+:             : meta.class.python, punctuation.section.class.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
