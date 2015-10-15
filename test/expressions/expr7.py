5142687a909f47e68ba29af2273fa5fe51f3c309
Yury Selivanov <yselivanov@sprymix.com>
2015-10-15 00:38:23 -0400

    Mark things like 'foo.None' as illegal

---
+++ test/expressions/expr7.py
@@ -0,0 +1,18 @@
+a.True = b.False = d.None
+
+
+
+a             : source.python
+.             : source.python
+True          : invalid.illegal.name.python, source.python
+              : source.python
+=             : keyword.operator.assignment.python, source.python
+ b            : source.python
+.             : source.python
+False         : invalid.illegal.name.python, source.python
+              : source.python
+=             : keyword.operator.assignment.python, source.python
+ d            : source.python
+.             : source.python
+None          : invalid.illegal.name.python, source.python
+              : source.python
