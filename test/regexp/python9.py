f43686fb11f47f713003d82317daa77e633d3e7a
Victor Petrovykh <vpetrovykh@sprymix.com>
2015-10-15 17:34:54 -0400

    Handle lone '[]' and '[^]' in regexp strings.

---
+++ test/regexp/python9.py
@@ -0,0 +1,14 @@
+r'foo[^]'
+r'foo[]'
+
+
+r             : source.python, storage.type.string.python, string.regexp.quoted.single.python
+'             : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.single.python
+foo           : source.python, string.regexp.quoted.single.python
+[^]           : source.python, string.regexp.quoted.single.python
+'             : punctuation.definition.string.end.python, source.python, string.regexp.quoted.single.python
+r             : source.python, storage.type.string.python, string.regexp.quoted.single.python
+'             : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.single.python
+foo           : source.python, string.regexp.quoted.single.python
+[]            : source.python, string.regexp.quoted.single.python
+'             : punctuation.definition.string.end.python, source.python, string.regexp.quoted.single.python
