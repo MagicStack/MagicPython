85784b0fc5e37de380ed8e74f42b32dc058f89f5
Victor Petrovykh <vpetrovykh@sprymix.com>
2015-10-14 21:05:15 -0400

    Add more tests for multiline regexp strings variations.

---
+++ test/regexp/python3.py
@@ -0,0 +1,53 @@
+r'''
+    (?x)
+        foo
+'''
+br'''
+    (?x)
+        foo
+'''
+rb'''
+    (?x)
+        foo
+'''
+Br'''
+    (?x)
+        foo
+'''
+rB'''
+    (?x)
+        foo
+'''
+
+
+
+r             : source.python, storage.type.string.python, string.regexp.quoted.triple.python
+'''           : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.triple.python
+              : source.python, string.regexp.quoted.triple.python
+(?x)          : source.python, storage.modifier.flag.regexp, string.regexp.quoted.triple.python
+        foo   : source.python, string.regexp.quoted.triple.python
+'''           : punctuation.definition.string.end.python, source.python, string.regexp.quoted.triple.python
+br            : source.python, storage.type.string.python, string.regexp.quoted.triple.python
+'''           : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.triple.python
+              : source.python, string.regexp.quoted.triple.python
+(?x)          : source.python, storage.modifier.flag.regexp, string.regexp.quoted.triple.python
+        foo   : source.python, string.regexp.quoted.triple.python
+'''           : punctuation.definition.string.end.python, source.python, string.regexp.quoted.triple.python
+rb            : source.python, storage.type.string.python, string.regexp.quoted.triple.python
+'''           : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.triple.python
+              : source.python, string.regexp.quoted.triple.python
+(?x)          : source.python, storage.modifier.flag.regexp, string.regexp.quoted.triple.python
+        foo   : source.python, string.regexp.quoted.triple.python
+'''           : punctuation.definition.string.end.python, source.python, string.regexp.quoted.triple.python
+Br            : source.python, storage.type.string.python, string.regexp.quoted.triple.python
+'''           : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.triple.python
+              : source.python, string.regexp.quoted.triple.python
+(?x)          : source.python, storage.modifier.flag.regexp, string.regexp.quoted.triple.python
+        foo   : source.python, string.regexp.quoted.triple.python
+'''           : punctuation.definition.string.end.python, source.python, string.regexp.quoted.triple.python
+rB            : source.python, storage.type.string.python, string.regexp.quoted.triple.python
+'''           : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.triple.python
+              : source.python, string.regexp.quoted.triple.python
+(?x)          : source.python, storage.modifier.flag.regexp, string.regexp.quoted.triple.python
+        foo   : source.python, string.regexp.quoted.triple.python
+'''           : punctuation.definition.string.end.python, source.python, string.regexp.quoted.triple.python
