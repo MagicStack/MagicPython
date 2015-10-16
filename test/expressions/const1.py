ea1f959cbabb1d8514bd3bb43e94f7cfe4a03a1e
Yury Selivanov <yselivanov@sprymix.com>
2015-10-16 01:26:12 -0400

    Recognize names like '_A' as constants

--- test/expressions/const1.py
+++ test/expressions/const1.py
@@ -1,5 +1,5 @@
-QQQQ QQQQ_123 QQQQ123 PROTOCOL_v2 QQQ.bar baz.AA_a
-QQQq QQQq123 self.FOOO()
+QQQQ QQQQ_123 QQQQ123 PROTOCOL_v2 QQQ.bar baz.AA_a _AAA foo._AAA _A
+QQQq QQQq123 self.FOOO() _ _1 _1A _a __
 
 
 
@@ -19,7 +19,14 @@ baz           : source.python
 .             : source.python
 AA_a          : constant.other.caps.python, source.python
               : source.python
+_AAA          : constant.other.caps.python, source.python
+ foo          : source.python
+.             : source.python
+_AAA          : constant.other.caps.python, source.python
+              : source.python
+_A            : constant.other.caps.python, source.python
 QQQq QQQq123 self. : source.python
 FOOO          : meta.function-call.python, source.python
 (             : meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
 )             : meta.function-call.python, punctuation.definition.arguments.end.python, source.python
+ _ _1 _1A _a __ : source.python
