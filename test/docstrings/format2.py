8fda6c50b688040641f2a1ea6f21a34ed84a3c7a
Yury Selivanov <yselivanov@sprymix.com>
2015-10-17 00:52:57 -0400

    Disable {..} formatting for docstrings

---
+++ test/docstrings/format2.py
@@ -0,0 +1,8 @@
+'''{foo}'''
+
+
+
+
+'''           : punctuation.definition.string.begin.python, source.python, string.quoted.docstring.python
+{foo}         : source.python, string.quoted.docstring.python
+'''           : punctuation.definition.string.end.python, source.python, string.quoted.docstring.python
