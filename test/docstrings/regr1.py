325d8dfc3a24d86ac4c10b603d3e131d99935f93
Victor Petrovykh <victor@magic.io>
2015-10-19 18:24:57 -0400

    Fix highlighting of decorators preceded by ':'

---
+++ test/docstrings/regr1.py
@@ -0,0 +1,21 @@
+#:
+    @asd
+    def foo():
+        pass
+
+
+
+#             : comment.line.number-sign.python, punctuation.definition.comment.python, source.python
+:             : comment.line.number-sign.python, source.python
+              : meta.function.decorator.python, source.python
+@             : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+asd           : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+              : meta.function.python, source.python
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+foo           : entity.name.function.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
