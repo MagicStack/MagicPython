a0236389c0eba710ae411ccd3f5701cb5bb2c399
Yury Selivanov <yury@magic.io>
2015-10-19 10:38:10 -0400

    Add tests for global & nonlocal

---
+++ test/statements/global1.py
@@ -0,0 +1,6 @@
+global a, b, c
+
+
+
+global        : source.python, storage.modifier.declaration.python
+ a, b, c      : source.python
---
+++ test/statements/nonlocal1.py
@@ -0,0 +1,6 @@
+nonlocal a, b, c
+
+
+
+nonlocal      : source.python, storage.modifier.declaration.python
+ a, b, c      : source.python
