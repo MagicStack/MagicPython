b4b308328799f64e62c671e74c5655b7ee392b6f
Yury Selivanov <yury@magic.io>
2015-10-19 00:00:31 -0400

    Support 'from...import' syntax (no whitespace)

---
+++ test/statements/import3.py
@@ -0,0 +1,18 @@
+from....import a
+from...import b
+from..import c
+
+
+
+from          : keyword.control.flow.python, source.python
+....          : source.python
+import        : keyword.control.flow.python, source.python
+ a            : source.python
+from          : keyword.control.flow.python, source.python
+...           : source.python
+import        : keyword.control.flow.python, source.python
+ b            : source.python
+from          : keyword.control.flow.python, source.python
+..            : source.python
+import        : keyword.control.flow.python, source.python
+ c            : source.python
