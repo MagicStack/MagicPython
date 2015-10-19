31c49459df54a920d147c94877df571620cf0ef9
Yury Selivanov <yury@magic.io>
2015-10-18 21:57:15 -0400

    Treat 'yield from' as one token

---
+++ test/expressions/expr9.py
@@ -0,0 +1,9 @@
+yield  from a
+yield a
+
+
+
+yield  from   : keyword.operator.python, source.python
+ a            : source.python
+yield         : keyword.operator.python, source.python
+ a            : source.python
