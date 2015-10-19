0291ac95161db254e5daa111670c422fdd2b1571
Yury Selivanov <yury@magic.io>
2015-10-18 21:56:36 -0400

    Add a test for the assert statement

---
+++ test/expressions/expr8.py
@@ -0,0 +1,11 @@
+assert a or b, 'aaa'
+
+
+
+assert        : keyword.control.flow.python, source.python
+ a            : source.python
+or            : keyword.operator.python, source.python
+ b,           : source.python
+'             : punctuation.definition.string.begin.python, source.python, string.quoted.single.python
+aaa           : source.python, string.quoted.single.python
+'             : punctuation.definition.string.end.python, source.python, string.quoted.single.python
