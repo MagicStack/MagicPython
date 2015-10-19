e1532445ae72272854ad86f2e25829a58281ee09
Victor Petrovykh <vpetrovykh@sprymix.com>
2015-10-19 17:00:16 -0400

    Fix illegan mismatched closing quote for one-line regexp.

---
+++ test/regexp/python10.py
@@ -0,0 +1,124 @@
+a = r'(<\' for
+a = r"(<\" for
+a = r'[<\' for
+a = r"[<\" for
+a = r'(?=\' for
+a = r"(?=\" for
+a = r'(?P<a>\' for
+a = r"(?P<a>\" for
+a = r'(?<!a\' for
+a = r"(?<!a\" for
+return some
+
+
+
+a             : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+r             : source.python, storage.type.string.python, string.regexp.quoted.python
+'             : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.python
+(             : punctuation.parenthesis.begin.regexp, source.python, string.regexp.quoted.python, support.other.parenthesis.regexp
+<             : source.python, string.regexp.quoted.python
+\'            : constant.character.escape.regexp, source.python, string.regexp.quoted.python
+ for          : source.python, string.regexp.quoted.python
+              : invalid.illegal.newline.python, source.python, string.regexp.quoted.python
+a             : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+r             : source.python, storage.type.string.python, string.regexp.quoted.python
+"             : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.python
+(             : punctuation.parenthesis.begin.regexp, source.python, string.regexp.quoted.python, support.other.parenthesis.regexp
+<             : source.python, string.regexp.quoted.python
+\"            : constant.character.escape.regexp, source.python, string.regexp.quoted.python
+ for          : source.python, string.regexp.quoted.python
+              : invalid.illegal.newline.python, source.python, string.regexp.quoted.python
+a             : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+r             : source.python, storage.type.string.python, string.regexp.quoted.python
+'             : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.python
+[             : constant.other.set.regexp, meta.character.set.regexp, punctuation.character.set.begin.regexp, source.python, string.regexp.quoted.python
+<             : constant.character.set.regexp, meta.character.set.regexp, source.python, string.regexp.quoted.python
+\'            : constant.character.escape.regexp, meta.character.set.regexp, source.python, string.regexp.quoted.python
+              : constant.character.set.regexp, meta.character.set.regexp, source.python, string.regexp.quoted.python
+f             : constant.character.set.regexp, meta.character.set.regexp, source.python, string.regexp.quoted.python
+o             : constant.character.set.regexp, meta.character.set.regexp, source.python, string.regexp.quoted.python
+r             : constant.character.set.regexp, meta.character.set.regexp, source.python, string.regexp.quoted.python
+              : invalid.illegal.newline.python, source.python, string.regexp.quoted.python
+a             : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+r             : source.python, storage.type.string.python, string.regexp.quoted.python
+"             : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.python
+[             : constant.other.set.regexp, meta.character.set.regexp, punctuation.character.set.begin.regexp, source.python, string.regexp.quoted.python
+<             : constant.character.set.regexp, meta.character.set.regexp, source.python, string.regexp.quoted.python
+\"            : constant.character.escape.regexp, meta.character.set.regexp, source.python, string.regexp.quoted.python
+              : constant.character.set.regexp, meta.character.set.regexp, source.python, string.regexp.quoted.python
+f             : constant.character.set.regexp, meta.character.set.regexp, source.python, string.regexp.quoted.python
+o             : constant.character.set.regexp, meta.character.set.regexp, source.python, string.regexp.quoted.python
+r             : constant.character.set.regexp, meta.character.set.regexp, source.python, string.regexp.quoted.python
+              : invalid.illegal.newline.python, source.python, string.regexp.quoted.python
+a             : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+r             : source.python, storage.type.string.python, string.regexp.quoted.python
+'             : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.python
+(             : keyword.operator.lookahead.regexp, punctuation.parenthesis.lookahead.begin.regexp, source.python, string.regexp.quoted.python
+?=            : keyword.operator.lookahead.regexp, source.python, string.regexp.quoted.python
+\'            : constant.character.escape.regexp, source.python, string.regexp.quoted.python
+ for          : source.python, string.regexp.quoted.python
+              : invalid.illegal.newline.python, source.python, string.regexp.quoted.python
+a             : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+r             : source.python, storage.type.string.python, string.regexp.quoted.python
+"             : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.python
+(             : keyword.operator.lookahead.regexp, punctuation.parenthesis.lookahead.begin.regexp, source.python, string.regexp.quoted.python
+?=            : keyword.operator.lookahead.regexp, source.python, string.regexp.quoted.python
+\"            : constant.character.escape.regexp, source.python, string.regexp.quoted.python
+ for          : source.python, string.regexp.quoted.python
+              : invalid.illegal.newline.python, source.python, string.regexp.quoted.python
+a             : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+r             : source.python, storage.type.string.python, string.regexp.quoted.python
+'             : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.python
+(             : meta.named.regexp, punctuation.parenthesis.named.begin.regexp, source.python, string.regexp.quoted.python, support.other.parenthesis.regexp
+?P<a>         : entity.name.tag.named.group.regexp, meta.named.regexp, source.python, string.regexp.quoted.python
+\'            : constant.character.escape.regexp, meta.named.regexp, source.python, string.regexp.quoted.python
+ for          : meta.named.regexp, source.python, string.regexp.quoted.python
+              : invalid.illegal.newline.python, source.python, string.regexp.quoted.python
+a             : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+r             : source.python, storage.type.string.python, string.regexp.quoted.python
+"             : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.python
+(             : meta.named.regexp, punctuation.parenthesis.named.begin.regexp, source.python, string.regexp.quoted.python, support.other.parenthesis.regexp
+?P<a>         : entity.name.tag.named.group.regexp, meta.named.regexp, source.python, string.regexp.quoted.python
+\"            : constant.character.escape.regexp, meta.named.regexp, source.python, string.regexp.quoted.python
+ for          : meta.named.regexp, source.python, string.regexp.quoted.python
+              : invalid.illegal.newline.python, source.python, string.regexp.quoted.python
+a             : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+r             : source.python, storage.type.string.python, string.regexp.quoted.python
+'             : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.python
+(             : keyword.operator.lookbehind.negative.regexp, punctuation.parenthesis.lookbehind.begin.regexp, source.python, string.regexp.quoted.python
+?<!           : keyword.operator.lookbehind.negative.regexp, source.python, string.regexp.quoted.python
+a             : source.python, string.regexp.quoted.python
+\'            : constant.character.escape.regexp, source.python, string.regexp.quoted.python
+ for          : source.python, string.regexp.quoted.python
+              : invalid.illegal.newline.python, source.python, string.regexp.quoted.python
+a             : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+r             : source.python, storage.type.string.python, string.regexp.quoted.python
+"             : punctuation.definition.string.begin.python, source.python, string.regexp.quoted.python
+(             : keyword.operator.lookbehind.negative.regexp, punctuation.parenthesis.lookbehind.begin.regexp, source.python, string.regexp.quoted.python
+?<!           : keyword.operator.lookbehind.negative.regexp, source.python, string.regexp.quoted.python
+a             : source.python, string.regexp.quoted.python
+\"            : constant.character.escape.regexp, source.python, string.regexp.quoted.python
+ for          : source.python, string.regexp.quoted.python
+              : invalid.illegal.newline.python, source.python, string.regexp.quoted.python
+return        : keyword.control.flow.python, source.python
+ some         : source.python
