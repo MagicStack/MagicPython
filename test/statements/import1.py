7bbbc0823a6b4c966f5311f3568241326995eb73
Yury Selivanov <yury@magic.io>
2015-10-18 22:01:17 -0400

    Add tests for import statements

---
+++ test/statements/import1.py
@@ -0,0 +1,18 @@
+from ...foo import bar as spam, baz
+import time as ham, datetime
+
+
+
+from          : keyword.control.flow.python, source.python
+ ..           : source.python
+.             : source.python
+foo           : source.python
+              : source.python
+import        : keyword.control.flow.python, source.python
+ bar          : source.python
+as            : keyword.control.flow.python, source.python
+ spam, baz    : source.python
+import        : keyword.control.flow.python, source.python
+ time         : source.python
+as            : keyword.control.flow.python, source.python
+ ham, datetime : source.python
