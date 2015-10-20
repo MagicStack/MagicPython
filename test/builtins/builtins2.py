67c3322fb43b01d94424a94ba6a0cba2aced6b1c
Yury Selivanov <yury@magic.io>
2015-10-20 12:50:04 -0400

    Scope python2-only builtins with variable.legacy.builtin.python

--- test/builtins/builtins2.py
+++ test/builtins/builtins2.py
@@ -1,7 +1,25 @@
 Error
-file reduce intern raw_input unicode cmp basestring
+file reduce intern raw_input unicode cmp basestring execfile long xrange
 
 
 
 Error         : source.python
-file reduce intern raw_input unicode cmp basestring : source.python
+file          : source.python, variable.legacy.builtin.python
+              : source.python
+reduce        : source.python, variable.legacy.builtin.python
+              : source.python
+intern        : source.python, variable.legacy.builtin.python
+              : source.python
+raw_input     : source.python, variable.legacy.builtin.python
+              : source.python
+unicode       : source.python, variable.legacy.builtin.python
+              : source.python
+cmp           : source.python, variable.legacy.builtin.python
+              : source.python
+basestring    : source.python, variable.legacy.builtin.python
+              : source.python
+execfile      : source.python, variable.legacy.builtin.python
+              : source.python
+long          : source.python, variable.legacy.builtin.python
+              : source.python
+xrange        : source.python, variable.legacy.builtin.python
---
+++ test/builtins/builtins4.py
@@ -0,0 +1,33 @@
+some.int
+some.sum
+some.super
+some.unicode
+some.foo
+some.Exception
+
+
+
+some          : source.python
+.             : source.python
+int           : source.python
+              : source.python
+some          : source.python
+.             : source.python
+sum           : source.python
+              : source.python
+some          : source.python
+.             : source.python
+super         : source.python
+              : source.python
+some          : source.python
+.             : source.python
+unicode       : source.python
+              : source.python
+some          : source.python
+.             : source.python
+foo           : source.python
+              : source.python
+some          : source.python
+.             : source.python
+Exception     : source.python
+              : source.python
