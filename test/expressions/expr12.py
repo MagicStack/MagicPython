adeb529865127604a914139926e7e8c302406a71
Yury Selivanov <yury@magic.io>
2015-10-20 11:56:51 -0400

    Highlight built-in functions before [] (for Python 2 print statement etc)

---
+++ test/expressions/expr12.py
@@ -0,0 +1,17 @@
+print []
+print {}
+print 1
+
+
+
+print         : meta.item-access.python, source.python, support.function.builtin.python
+              : meta.item-access.python, source.python
+[             : meta.item-access.arguments.python, meta.item-access.python, punctuation.definition.arguments.begin.python, source.python
+]             : meta.item-access.python, punctuation.definition.arguments.end.python, source.python
+print         : source.python, support.function.builtin.python
+              : source.python
+{             : punctuation.definition.dict.begin.python, source.python
+}             : punctuation.definition.dict.end.python, source.python
+print         : source.python, support.function.builtin.python
+              : source.python
+1             : constant.numeric.dec.python, source.python
