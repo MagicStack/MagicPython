204d428e6d465c8de72251f65a4ca5cce149b0bd
Yury Selivanov <yury@magic.io>
2015-10-20 12:53:39 -0400

    Test that Python2 '<>' scopes OK

---
+++ test/expressions/expr14.py
@@ -0,0 +1,8 @@
+a <> b
+
+
+
+a             : source.python
+<             : keyword.operator.python, source.python
+>             : keyword.operator.python, source.python
+ b            : source.python
