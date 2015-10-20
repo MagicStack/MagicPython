bff4e71412abea1699dbb51215fbdcba60c67df2
Yury Selivanov <yury@magic.io>
2015-10-20 12:16:15 -0400

    Scope 'super' with support.type.python

---
+++ test/expressions/expr13.py
@@ -0,0 +1,7 @@
+super()
+
+
+
+super         : meta.function-call.python, source.python, support.type.python
+(             : meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+)             : meta.function-call.python, punctuation.definition.arguments.end.python, source.python
