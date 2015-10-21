0935b72d7301a251a0d38dba44df3c22dd8eaa08
Yury Selivanov <yury@magic.io>
2015-10-20 23:46:36 -0400

    Don't highlight lone '...' and '>>>' in docstrings

---
+++ test/docstrings/prompt2.py
@@ -0,0 +1,13 @@
+"""
+    def foo():
+        ...
+        >>>
+"""
+
+
+
+"""           : punctuation.definition.string.begin.python, source.python, string.quoted.docstring.multi.python
+    def foo(): : source.python, string.quoted.docstring.multi.python
+        ...   : source.python, string.quoted.docstring.multi.python
+        >>>   : source.python, string.quoted.docstring.multi.python
+"""           : punctuation.definition.string.end.python, source.python, string.quoted.docstring.multi.python
---
+++ test/docstrings/prompt3.py
@@ -0,0 +1,9 @@
+'''...'''
+...
+
+
+
+'''           : punctuation.definition.string.begin.python, source.python, string.quoted.docstring.multi.python
+...           : source.python, string.quoted.docstring.multi.python
+'''           : punctuation.definition.string.end.python, source.python, string.quoted.docstring.multi.python
+...           : constant.other.ellipsis.python, source.python
