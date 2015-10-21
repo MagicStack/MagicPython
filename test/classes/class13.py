c02533593833c75b4f1d03339748da1b72f2ed26
Yury Selivanov <yury@magic.io>
2015-10-21 00:00:59 -0400

    Add a test that we support unicode just fine

---
+++ test/classes/class13.py
@@ -0,0 +1,67 @@
+class Üa(Êa):
+    'Œ\nŃ'
+    @æaœ
+    def ŌÏŒĘ(self, Ú=1):
+        print('превед 你好')
+        return Ù
+    你好 = lambda: 你好
+    def 你好(): pass
+
+
+
+class         : meta.class.python, source.python, storage.type.class.python
+              : meta.class.python, source.python
+Üa            : entity.name.type.class.python, meta.class.python, source.python
+(             : meta.class.inheritance.python, meta.class.python, punctuation.definition.inheritance.begin.python, source.python
+Êa            : entity.other.inherited-class.python, meta.class.inheritance.python, meta.class.python, source.python
+)             : meta.class.inheritance.python, meta.class.python, punctuation.definition.inheritance.end.python, source.python
+:             : meta.class.python, punctuation.section.class.begin.python, source.python
+              : source.python
+'             : punctuation.definition.string.begin.python, source.python, string.quoted.docstring.python
+Œ             : source.python, string.quoted.docstring.python
+\n            : constant.character.python, source.python, string.quoted.docstring.python
+Ń             : source.python, string.quoted.docstring.python
+'             : punctuation.definition.string.end.python, source.python, string.quoted.docstring.python
+              : meta.function.decorator.python, source.python
+@             : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+æaœ           : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+              : meta.function.python, source.python
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+ŌÏŒĘ          : entity.name.function.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+self          : meta.function.parameters.python, meta.function.python, source.python, variable.parameter.function.language.python, variable.parameter.function.language.special.self.python
+,             : meta.function.parameters.python, meta.function.python, punctuation.separator.parameters.python, source.python
+              : meta.function.parameters.python, meta.function.python, source.python
+Ú             : meta.function.parameters.python, meta.function.python, source.python, variable.parameter.function.language.python
+=             : keyword.operator.python, meta.function.parameters.python, meta.function.python, source.python
+1             : constant.numeric.dec.python, meta.function.parameters.python, meta.function.python, source.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
+              : source.python
+print         : meta.function-call.python, source.python, support.function.builtin.python
+(             : meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+'             : meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.string.begin.python, source.python, string.quoted.single.python
+превед 你好     : meta.function-call.arguments.python, meta.function-call.python, source.python, string.quoted.single.python
+'             : meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.string.end.python, source.python, string.quoted.single.python
+)             : meta.function-call.python, punctuation.definition.arguments.end.python, source.python
+              : source.python
+return        : keyword.control.flow.python, source.python
+              : source.python
+Ù             : constant.other.caps.python, source.python
+    你好        : source.python
+=             : keyword.operator.assignment.python, source.python
+              : source.python
+lambda        : meta.lambda-function.python, source.python, storage.type.function.lambda.python
+:             : meta.lambda-function.python, punctuation.section.function.lambda.begin.python, source.python
+              : source.python
+你好            : source.python
+              : meta.function.python, source.python
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+你好            : entity.name.function.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
