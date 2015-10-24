a304f636bf2174f805929f88c4f023c2818b360f
Yury Selivanov <yury@magic.io>
2015-10-24 03:03:33 -0400

    Add a couple of tests for args unpacking

---
+++ test/calls/call5.py
@@ -0,0 +1,15 @@
+f(*a)
+f(**a)
+
+
+
+f             : meta.function-call.generic.python, meta.function-call.python, source.python
+(             : meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+*             : keyword.operator.unpacking.arguments.python, meta.function-call.python, source.python
+a             : meta.function-call.arguments.python, meta.function-call.python, source.python
+)             : meta.function-call.python, punctuation.definition.arguments.end.python, source.python
+f             : meta.function-call.generic.python, meta.function-call.python, source.python
+(             : meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+**            : keyword.operator.unpacking.arguments.python, meta.function-call.python, source.python
+a             : meta.function-call.arguments.python, meta.function-call.python, source.python
+)             : meta.function-call.python, punctuation.definition.arguments.end.python, source.python
---
+++ test/calls/call6.py
@@ -0,0 +1,27 @@
+f(  *a ,  *())
+f(  **a ,  **{})
+
+
+
+f             : meta.function-call.generic.python, meta.function-call.python, source.python
+(             : meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+              : meta.function-call.python, source.python
+*             : keyword.operator.unpacking.arguments.python, meta.function-call.python, source.python
+a             : meta.function-call.arguments.python, meta.function-call.python, source.python
+,             : meta.function-call.arguments.python, meta.function-call.python, punctuation.separator.arguments.python, source.python
+              : meta.function-call.arguments.python, meta.function-call.python, source.python
+*             : keyword.operator.unpacking.arguments.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+(             : meta.function-call.arguments.python, meta.function-call.python, punctuation.parenthesis.begin.python, source.python
+)             : meta.function-call.arguments.python, meta.function-call.python, punctuation.parenthesis.end.python, source.python
+)             : meta.function-call.python, punctuation.definition.arguments.end.python, source.python
+f             : meta.function-call.generic.python, meta.function-call.python, source.python
+(             : meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+              : meta.function-call.python, source.python
+**            : keyword.operator.unpacking.arguments.python, meta.function-call.python, source.python
+a             : meta.function-call.arguments.python, meta.function-call.python, source.python
+,             : meta.function-call.arguments.python, meta.function-call.python, punctuation.separator.arguments.python, source.python
+              : meta.function-call.arguments.python, meta.function-call.python, source.python
+**            : keyword.operator.unpacking.arguments.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+{             : meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.dict.begin.python, source.python
+}             : meta.function-call.arguments.python, meta.function-call.python, punctuation.definition.dict.end.python, source.python
+)             : meta.function-call.python, punctuation.definition.arguments.end.python, source.python
