9b3681af1fe2b20feca99800d06db078de372338
Yury Selivanov <yury@magic.io>
2015-10-30 20:22:08 -0400

    Fix function calls highlighting

---
+++ test/expressions/expr16.py
@@ -0,0 +1,11 @@
+foo(djsgfjs==123)
+
+
+
+foo           : meta.function-call.generic.python, meta.function-call.python, source.python
+(             : meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+djsgfjs       : meta.function-call.arguments.python, meta.function-call.python, source.python
+=             : keyword.operator.assignment.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+=             : keyword.operator.assignment.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+123           : constant.numeric.dec.python, meta.function-call.arguments.python, meta.function-call.python, source.python
+)             : meta.function-call.python, punctuation.definition.arguments.end.python, source.python
