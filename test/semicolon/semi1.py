3aabae0a0c10d34de8f3775098df5ca517870ceb
Yury Selivanov <yury@magic.io>
2015-10-30 20:38:48 -0400

    Highlight trailing semicolons as invalid.deprecated

---
+++ test/semicolon/semi1.py
@@ -0,0 +1,13 @@
+a;
+;
+a=1; b
+
+
+
+a             : source.python
+;             : invalid.deprecated.semicolon.python, source.python
+;             : invalid.deprecated.semicolon.python, source.python
+a             : source.python
+=             : keyword.operator.assignment.python, source.python
+1             : constant.numeric.dec.python, source.python
+; b           : source.python
