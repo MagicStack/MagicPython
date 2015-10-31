9659a5bda17ddff9be49fcf0677bc55260cc294a
Yury Selivanov <yury@magic.io>
2015-10-31 14:46:46 -0400

    Don't specially highlight things like "something.Exception"

---
+++ test/classes/class14.py
@@ -0,0 +1,34 @@
+class F(f.Exception, f.type, Exception, Exception.a, b=Exception):
+    pass
+
+
+
+class         : meta.class.python, source.python, storage.type.class.python
+              : meta.class.python, source.python
+F             : entity.name.type.class.python, meta.class.python, source.python
+(             : meta.class.inheritance.python, meta.class.python, punctuation.definition.inheritance.begin.python, source.python
+f             : entity.other.inherited-class.python, meta.class.inheritance.python, meta.class.python, source.python
+.             : meta.class.inheritance.python, meta.class.python, source.python
+Exception     : entity.other.inherited-class.python, meta.class.inheritance.python, meta.class.python, source.python
+,             : meta.class.inheritance.python, meta.class.python, punctuation.separator.inheritance.python, source.python
+              : meta.class.inheritance.python, meta.class.python, source.python
+f             : entity.other.inherited-class.python, meta.class.inheritance.python, meta.class.python, source.python
+.             : meta.class.inheritance.python, meta.class.python, source.python
+type          : entity.other.inherited-class.python, meta.class.inheritance.python, meta.class.python, source.python
+,             : meta.class.inheritance.python, meta.class.python, punctuation.separator.inheritance.python, source.python
+              : meta.class.inheritance.python, meta.class.python, source.python
+Exception     : meta.class.inheritance.python, meta.class.python, source.python, support.type.exception.python
+,             : meta.class.inheritance.python, meta.class.python, punctuation.separator.inheritance.python, source.python
+              : meta.class.inheritance.python, meta.class.python, source.python
+Exception     : meta.class.inheritance.python, meta.class.python, source.python, support.type.exception.python
+.             : meta.class.inheritance.python, meta.class.python, source.python
+a             : entity.other.inherited-class.python, meta.class.inheritance.python, meta.class.python, source.python
+,             : meta.class.inheritance.python, meta.class.python, punctuation.separator.inheritance.python, source.python
+              : meta.class.inheritance.python, meta.class.python, source.python
+b             : entity.other.inherited-class.python, meta.class.inheritance.python, meta.class.python, source.python
+=             : keyword.operator.assignment.python, meta.class.inheritance.python, meta.class.python, source.python
+Exception     : meta.class.inheritance.python, meta.class.python, source.python, support.type.exception.python
+)             : meta.class.inheritance.python, meta.class.python, punctuation.definition.inheritance.end.python, source.python
+:             : meta.class.python, punctuation.section.class.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
---
+++ test/expressions/expr18.py
@@ -0,0 +1,13 @@
+a.Exception
+Exception.a
+
+
+
+a             : source.python
+.             : source.python
+Exception     : source.python
+              : source.python
+Exception     : source.python, support.type.exception.python
+.             : source.python
+a             : source.python
+              : source.python
