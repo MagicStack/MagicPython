c4d768ae573d2484e69a33ac3c842ed483ba7b1c
Yury Selivanov <yury@magic.io>
2015-10-30 20:05:37 -0400

    Fix regex for 'def' and 'async def'

---
+++ test/functions/async3.py
@@ -0,0 +1,14 @@
+casync def foo(): pass
+
+
+
+casync        : source.python
+              : meta.function.python, source.python
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+foo           : entity.name.function.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
---
+++ test/functions/decl9.py
@@ -0,0 +1,10 @@
+cdef foo(): pass
+
+
+
+cdef          : source.python
+foo           : meta.function-call.generic.python, meta.function-call.python, source.python
+(             : meta.function-call.python, punctuation.definition.arguments.begin.python, source.python
+)             : meta.function-call.python, punctuation.definition.arguments.end.python, source.python
+:             : source.python
+pass          : keyword.control.flow.python, source.python
