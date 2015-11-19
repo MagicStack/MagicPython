652811554d239c938af52ffb426a16345f4b5aef
Victor Petrovykh <vpetrovykh@sprymix.com>
2015-11-19 12:37:57 -0500

    Fix odd highlighting of strings and comments after '.'.

---
+++ test/expressions/expr19.py
@@ -0,0 +1,44 @@
+a. #foo
+a.
+#foo
+a. \
+#foo
+a. 'bar'
+a.
+'bar'
+a. \
+'bar'
+
+
+
+a             : source.python
+.             : source.python
+#             : comment.line.number-sign.python, punctuation.definition.comment.python, source.python
+foo           : comment.line.number-sign.python, source.python
+a             : source.python
+.             : source.python
+#             : comment.line.number-sign.python, punctuation.definition.comment.python, source.python
+foo           : comment.line.number-sign.python, source.python
+a             : source.python
+.             : source.python
+\             : separator.continuation.line.python, source.python
+              : source.python
+#             : comment.line.number-sign.python, punctuation.definition.comment.python, source.python
+foo           : comment.line.number-sign.python, source.python
+a             : source.python
+.             : source.python
+'             : punctuation.definition.string.begin.python, source.python, string.quoted.single.python
+bar           : source.python, string.quoted.single.python
+'             : punctuation.definition.string.end.python, source.python, string.quoted.single.python
+a             : source.python
+.             : source.python
+'             : punctuation.definition.string.begin.python, source.python, string.quoted.docstring.single.python
+bar           : source.python, string.quoted.docstring.single.python
+'             : punctuation.definition.string.end.python, source.python, string.quoted.docstring.single.python
+a             : source.python
+.             : source.python
+\             : separator.continuation.line.python, source.python
+              : source.python
+'             : punctuation.definition.string.begin.python, source.python, string.quoted.single.python
+bar           : source.python, string.quoted.single.python
+'             : punctuation.definition.string.end.python, source.python, string.quoted.single.python
