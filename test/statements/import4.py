8717dd5cedb320014503cf7ddd341023093f4c9e
Victor Petrovykh <vpetrovykh@sprymix.com>
2016-03-03 16:18:47 -0500

    Add some more tests for 'import' and 'from' keywords.

---
+++ test/statements/import4.py
@@ -0,0 +1,35 @@
+from....foo import a
+from...foo import b
+from..foo import c
+from.foo import d
+
+
+
+from          : keyword.control.import.python, source.python
+....          : source.python
+foo           : source.python
+              : source.python
+import        : keyword.control.import.python, source.python
+              : source.python
+a             : source.python
+from          : keyword.control.import.python, source.python
+...           : source.python
+foo           : source.python
+              : source.python
+import        : keyword.control.import.python, source.python
+              : source.python
+b             : source.python
+from          : keyword.control.import.python, source.python
+..            : source.python
+foo           : source.python
+              : source.python
+import        : keyword.control.import.python, source.python
+              : source.python
+c             : source.python
+from          : keyword.control.import.python, source.python
+.             : source.python
+foo           : source.python
+              : source.python
+import        : keyword.control.import.python, source.python
+              : source.python
+d             : source.python
---
+++ test/statements/import5.py
@@ -0,0 +1,47 @@
+from.foo import d
+from.import a
+
+foo.import
+
+raise Exception from Foo
+
+def bar():
+    yield from baz
+
+
+
+from          : keyword.control.import.python, source.python
+.             : source.python
+foo           : source.python
+              : source.python
+import        : keyword.control.import.python, source.python
+              : source.python
+d             : source.python
+from          : keyword.control.import.python, source.python
+.             : source.python
+import        : keyword.control.import.python, source.python
+              : source.python
+a             : source.python
+              : source.python
+foo           : source.python
+.             : source.python
+import        : keyword.control.flow.python, source.python
+              : source.python
+raise         : keyword.control.flow.python, source.python
+              : source.python
+Exception     : source.python, support.type.exception.python
+              : source.python
+from          : keyword.control.flow.python, source.python
+              : source.python
+Foo           : source.python
+              : source.python
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+bar           : entity.name.function.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
+              : source.python
+yield from    : keyword.control.flow.python, source.python
+              : source.python
+baz           : source.python
