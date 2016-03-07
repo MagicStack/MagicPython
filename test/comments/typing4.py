7d07d6f5b052785603e1e8cfbf771ccd442d2975
Yury Selivanov <yury@magic.io>
2016-03-07 17:46:49 -0500

    Fix a typo in type-hints scope name (issue #37)

--- test/comments/typing4.py
+++ test/comments/typing4.py
@@ -13,12 +13,12 @@ None          : constant.language.python, source.python
 type:         : comment.line.number-sign.python, comment.typehint.directive.notation.python, meta.typehint.comment.python, source.python
               : comment.line.number-sign.python, meta.typehint.comment.python, source.python
 List          : comment.line.number-sign.python, comment.typehint.type.notation.python, meta.typehint.comment.python, source.python
-[             : comment.line.number-sign.python, comment.typehint.puctuation.notation.python, meta.typehint.comment.python, source.python
+[             : comment.line.number-sign.python, comment.typehint.punctuation.notation.python, meta.typehint.comment.python, source.python
 str           : comment.line.number-sign.python, comment.typehint.type.notation.python, meta.typehint.comment.python, source.python
-,             : comment.line.number-sign.python, comment.typehint.puctuation.notation.python, meta.typehint.comment.python, source.python
+,             : comment.line.number-sign.python, comment.typehint.punctuation.notation.python, meta.typehint.comment.python, source.python
               : comment.line.number-sign.python, meta.typehint.comment.python, source.python
 a             : comment.line.number-sign.python, comment.typehint.variable.notation.python, meta.typehint.comment.python, source.python
-]             : comment.line.number-sign.python, comment.typehint.puctuation.notation.python, meta.typehint.comment.python, source.python
+]             : comment.line.number-sign.python, comment.typehint.punctuation.notation.python, meta.typehint.comment.python, source.python
 y             : source.python
               : source.python
 =             : keyword.operator.assignment.python, source.python
@@ -29,12 +29,12 @@ None          : constant.language.python, source.python
 type:         : comment.line.number-sign.python, comment.typehint.directive.notation.python, meta.typehint.comment.python, source.python
               : comment.line.number-sign.python, meta.typehint.comment.python, source.python
 Dict          : comment.line.number-sign.python, comment.typehint.type.notation.python, meta.typehint.comment.python, source.python
-[             : comment.line.number-sign.python, comment.typehint.puctuation.notation.python, meta.typehint.comment.python, source.python
+[             : comment.line.number-sign.python, comment.typehint.punctuation.notation.python, meta.typehint.comment.python, source.python
 int           : comment.line.number-sign.python, comment.typehint.type.notation.python, meta.typehint.comment.python, source.python
-,             : comment.line.number-sign.python, comment.typehint.puctuation.notation.python, meta.typehint.comment.python, source.python
+,             : comment.line.number-sign.python, comment.typehint.punctuation.notation.python, meta.typehint.comment.python, source.python
               : comment.line.number-sign.python, meta.typehint.comment.python, source.python
 Any           : comment.line.number-sign.python, comment.typehint.type.notation.python, meta.typehint.comment.python, source.python
-]             : comment.line.number-sign.python, comment.typehint.puctuation.notation.python, meta.typehint.comment.python, source.python
+]             : comment.line.number-sign.python, comment.typehint.punctuation.notation.python, meta.typehint.comment.python, source.python
               : comment.line.number-sign.python, meta.typehint.comment.python, source.python
 #             : comment.line.number-sign.python, punctuation.definition.comment.python, source.python
  int          : comment.line.number-sign.python, source.python
