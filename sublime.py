021931bf47f24c2034323e8f30066e83300aec3d
Yury Selivanov <yury@magic.io>
2016-11-29 23:52:03 -0500

    sublime: Replace the f-string macro with a Python plugin.
    Macro approach is less reliable because the path to the macro
    file has to be hardcoded.  If MP is installed into a non-standard
    dir, the {} completion wouldn't work properly.

---
+++ sublime.py
@@ -0,0 +1,18 @@
+"""A plugin for Sublime Text to enhance f-string editing experience.
+
+Specifically, this plugin simplifies typing of escaped curly braces
+in an f-string:
+   {|}, where | is for cursir, gets replaced with
+   {{|, when '{' is typed again.
+"""
+
+
+import sublime
+import sublime_plugin
+
+
+class FstringbraceCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        view = self.window.active_view()
+        view.run_command('right_delete')
+        view.run_command('insert', {'characters': '{'})
