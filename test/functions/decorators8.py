b645cc70b09c489f1550e00ea99334aeb410ae3c
Yury Selivanov <yury@magic.io>
2016-11-30 21:19:57 -0500

    tests: Add a test for illegal decorating syntax

---
+++ test/functions/decorators8.py
@@ -0,0 +1,21 @@
+@deco().abc  # SyntaxError: invalid syntax
+def foo(): pass
+
+
+
+@             : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+deco          : entity.name.function.decorator.python, meta.function.decorator.python, source.python
+(             : meta.function.decorator.python, punctuation.definition.arguments.begin.python, source.python
+)             : meta.function.decorator.python, punctuation.definition.arguments.end.python, source.python
+.abc          : invalid.illegal.decorator.python, meta.function.decorator.python, source.python
+              : source.python
+#             : comment.line.number-sign.python, punctuation.definition.comment.python, source.python
+ SyntaxError: invalid syntax : comment.line.number-sign.python, source.python
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+foo           : entity.name.function.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
+              : source.python
+pass          : keyword.control.flow.python, source.python
