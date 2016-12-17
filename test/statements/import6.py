0cefbaa9355887ab1a03008f434ecb315bbe32ba
Yury Selivanov <yury@magic.io>
2016-12-17 16:33:18 -0500

    Add a test for "from __future__ import .."

---
+++ test/statements/import6.py
@@ -0,0 +1,11 @@
+from __future__ import generator_stop
+
+
+
+from          : keyword.control.flow.python, source.python
+              : source.python
+__future__    : source.python, support.variable.magic.python
+              : source.python
+import        : keyword.control.import.python, source.python
+              : source.python
+generator_stop : source.python
