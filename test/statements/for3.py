b4b2e6eb16fee36aea0788bf0aa1853c25f7d276
Ilia Novoselov <ilya.novoselov@gmail.com>
2020-05-18 23:51:23 +0500

    Fix for loop highlighting #211 (#212)

--- test/statements/for3.py
+++ test/statements/for3.py
@@ -1,21 +1,16 @@
-forvariable = None
-for a, b, c, invariable in [2 in q, 2 in w]:
+for(a, b), c, invariable in[2 in q, 2 in w]:
     pass
 
 
 
 
-forvariable   : source.python
-              : source.python
-=             : keyword.operator.assignment.python, source.python
-              : source.python
-None          : constant.language.python, source.python
 for           : keyword.control.flow.python, source.python
-              : source.python
+(             : punctuation.parenthesis.begin.python, source.python
 a             : source.python
 ,             : punctuation.separator.element.python, source.python
               : source.python
 b             : source.python
+)             : punctuation.parenthesis.end.python, source.python
 ,             : punctuation.separator.element.python, source.python
               : source.python
 c             : source.python
@@ -24,7 +19,6 @@ c             : source.python
 invariable    : source.python
               : source.python
 in            : keyword.control.flow.python, source.python
-              : source.python
 [             : punctuation.definition.list.begin.python, source.python
 2             : constant.numeric.dec.python, source.python
               : source.python
