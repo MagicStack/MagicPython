e3eeca8cc9402d0d668cb36ed444f0c916052409
Victor Petrovykh <victor@edgedb.com>
2020-07-31 05:08:47 -0400

    Add Python 3.8 features.
    Add walrus operator `:=`.
    Add positional only args separator `/`.
    Add `=` to f-string formatting.
    
    Fixes #189

---
+++ test/functions/decl15.py
@@ -0,0 +1,34 @@
+def showcase(a, b, /, c, d, *, e, f):
+    return
+
+
+
+
+def           : meta.function.python, source.python, storage.type.function.python
+              : meta.function.python, source.python
+showcase      : entity.name.function.python, meta.function.python, source.python
+(             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.begin.python, source.python
+a             : meta.function.parameters.python, meta.function.python, source.python, variable.parameter.function.language.python
+,             : meta.function.parameters.python, meta.function.python, punctuation.separator.parameters.python, source.python
+              : meta.function.parameters.python, meta.function.python, source.python
+b             : meta.function.parameters.python, meta.function.python, source.python, variable.parameter.function.language.python
+,             : meta.function.parameters.python, meta.function.python, punctuation.separator.parameters.python, source.python
+              : meta.function.parameters.python, meta.function.python, source.python
+/             : keyword.operator.positional.parameter.python, meta.function.parameters.python, meta.function.python, source.python
+,             : meta.function.parameters.python, meta.function.python, source.python
+c             : meta.function.parameters.python, meta.function.python, source.python, variable.parameter.function.language.python
+,             : meta.function.parameters.python, meta.function.python, punctuation.separator.parameters.python, source.python
+              : meta.function.parameters.python, meta.function.python, source.python
+d             : meta.function.parameters.python, meta.function.python, source.python, variable.parameter.function.language.python
+,             : meta.function.parameters.python, meta.function.python, punctuation.separator.parameters.python, source.python
+              : meta.function.parameters.python, meta.function.python, source.python
+*             : keyword.operator.unpacking.parameter.python, meta.function.parameters.python, meta.function.python, source.python
+,             : meta.function.parameters.python, meta.function.python, source.python
+e             : meta.function.parameters.python, meta.function.python, source.python, variable.parameter.function.language.python
+,             : meta.function.parameters.python, meta.function.python, punctuation.separator.parameters.python, source.python
+              : meta.function.parameters.python, meta.function.python, source.python
+f             : meta.function.parameters.python, meta.function.python, source.python, variable.parameter.function.language.python
+)             : meta.function.parameters.python, meta.function.python, punctuation.definition.parameters.end.python, source.python
+:             : meta.function.python, punctuation.section.function.begin.python, source.python
+              : source.python
+return        : keyword.control.flow.python, source.python
